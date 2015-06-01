29aca235b2286d8b48d795302302d7caca2c16a8
Jonathan Creasy <jonathan@ghostlab.net>
2015-06-01 18:12:31 -0500

    Cleanup of Executor code

--- executor.go
+++ executor.go
@@ -12,89 +12,103 @@ import (
   "github.com/elastic/libbeat/logp"
 )
 
-const CommandTimeout = 30 * time.Second
-const ReadInterval = 1 * time.Second
-const ReadTimeout = 1 * time.Second
-
 type Executor struct {
   Command   string /* the command to be run */
   Type      string /* the type to add to events */
+  CommandTimeout uint64 /* The number of seconds to allow the command to run */
+  ReadInterval uint64 /* The number of seconds to check the command output buffer */
+  ReadTimeout uint64 /* The amount of time to ry and read command output */
 }
 
 func (e *Executor) Execute(output chan common.MapStr) {
+  logp.Info("Preparing to start %s", e.Command)
+
   cmd := exec.Command(e.Command)
   stdoutbuffer := &bytes.Buffer{}
   cmd.Stdout = stdoutbuffer
 
-  // Start command asynchronously
+  // Execute the Command
   err := cmd.Start()
-  logp.Err("Error starting command: %s", err)
+  logp.Err("Could not start: %s", err)
 
-  // Create a ticker that outputs elapsed time
-  ticker := time.NewTicker(ReadInterval)
-  go func(ticker *time.Ticker, command string, event_type string, stdoutbuffer *bytes.Buffer, output chan common.MapStr) {
+  // Process the process output at every ReadInterval
+  ticker := time.NewTicker(e.ReadInterval * time.Second)
+  go func(ticker *time.Ticker, command string, event_type string, stdoutbuffer *bytes.Buffer, output chan common.MapStr, ReadTimeout time.Duration) {
     for _ = range ticker.C {
-      reader := bufio.NewReader(stdoutbuffer)
-      buffer := new(bytes.Buffer)
-
-      var offset int64 = 0
-      var line uint64 = 0
-
-      for {
-        text, bytesread, err := readline(reader, buffer, 1 * time.Second)
-        if err != nil {
-          if err == io.ErrUnexpectedEOF {
-            break
-          } else if err == io.EOF {
-            break
-          } else {
-            logp.Err("Unexpected state reading from stdout; error: %s\n", err)
-            return
-          }
-        }
-
-        event_timestamp := func() time.Time {
-          t := time.Now()
-          return t
-        }
-
-        line++
-
-        event := common.MapStr{}
-        event["source"] = command
-        event["offset"] = offset
-        event["length"] = bytesread
-        event["line"] = line
-        event["message"] = text
-        event["type"] = event_type
-
-        event.EnsureTimestampField(event_timestamp)
-        event.EnsureCountField()
-
-        offset += int64(bytesread)
-
-        output <- event // ship the new event downstream
-
-        logp.Info("\nOutput:\nBytes Read: %v\n%s", bytesread, *text)
-      }
+      processOutput(ticker, command, event_type, stdoutbuffer, output, ReadTimeout)
     }
-  }(ticker, e.Command, e.Type, stdoutbuffer, output)
+  }(ticker, e.Command, e.Type, stdoutbuffer, output, e.ReadTimeout * time.Second)
 
-  // Create a timer that will kill the process
-  timer := time.NewTimer(CommandTimeout)
+  // Kill the process after it has run for CommandTimeout
+  timer := time.NewTimer(e.CommandTimeout * time.Second)
   go func(timer *time.Timer, ticker *time.Ticker, cmd *exec.Cmd) {
     for _ = range timer.C {
-      err := cmd.Process.Signal(os.Kill)
-      logp.Err("Kill Process Error: %s", err)
-      ticker.Stop()
+      killCommand(timer, ticker, cmd)
     }
   }(timer, ticker, cmd)
 
   // Only proceed once the process has finished
   cmd.Wait()
-  logp.Info(fmt.Sprintf("%d bytes generated!", len(stdoutbuffer.Bytes())))
+  logp.Info("Command %s has ended", command)
 }
 
+func killCommand (timer *time.Timer, ticker *time.Ticker, cmd *exec.Cmd) {
+  err := cmd.Process.Signal(os.Kill)
+  if err != nil {
+    logp.Err("Kill Process Error: %s", err)
+  }
+  ticker.Stop()
+}
+
+func processOutput (ticker *time.Ticker, command string, event_type string, stdoutbuffer *bytes.Buffer, output chan common.MapStr, ReadTimeout time.Duration) {
+  reader := bufio.NewReader(stdoutbuffer)
+  buffer := new(bytes.Buffer)
+
+  var offset int64 = 0
+  var line uint64 = 0
+
+  for {
+    text, bytesread, err := readline(reader, buffer, ReadTimeout)
+    if err != nil {
+      if err == io.ErrUnexpectedEOF || err == io.EOF {
+        // EOF happens if the process has not written any output since the last time we checked
+        break
+      } else {
+        logp.Err("Unexpected state reading from stdout; error: %s\n", err)
+        return
+      }
+    }
+
+    event_timestamp := func() time.Time {
+      t := time.Now()
+      return t
+    }
+
+    line++
+
+    event := common.MapStr{}
+    event["source"] = command
+    event["offset"] = offset
+    event["length"] = bytesread
+    event["line"] = line
+    event["message"] = text
+    event["type"] = event_type
+
+    event.EnsureTimestampField(event_timestamp)
+    event.EnsureCountField()
+
+    offset += int64(bytesread)
+
+    output <- event // ship the new event downstream
+
+    logp.Info("\nOutput:\nBytes Read: %v\n%s", bytesread, *text)
+  }
+}
+
+/*
+* This function is based on the readline function in Logstash-Forwarder which is available under an Apache2 License
+* https://github.com/elastic/logstash-forwarder/blob/4b6c987646bdc199eabf9b8f2f5ad57ff860b28e/harvester.go#L127
+*/
 func readline(reader *bufio.Reader, buffer *bytes.Buffer, eof_timeout time.Duration) (*string, int, error) {
   var is_partial bool = true
   var newline_length int = 1
@@ -129,7 +143,7 @@ func readline(reader *bufio.Reader, buffer *bytes.Buffer, eof_timeout time.Durat
         }
         continue
       } else {
-        //emit("error: Harvester.readLine: %s", err.Error())
+        logp.Error("Executor.readLine: %s", err.Error())
         return nil, 0, err // TODO(sissel): don't do this?
       }
     }
